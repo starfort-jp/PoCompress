a6c500d318b54ebe36bfead5201b1c71ea1c2f23
starfort-jp <starfort@nifty.com>
2016-06-11 22:18:32 +0900

    Create Project
    Create Project on Github

---
+++ PoCompress/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace PoCompress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(514, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 82);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(508, 457);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(508, 19);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Poファイル|*.po|すべてのファイル|*.*";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(514, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Selection";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(514, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Mark Line";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(514, 125);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Remove";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(14, 13);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 53);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(14, 13);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(514, 170);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Save File";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 542);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ PoCompress/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PoCompress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> xListString = new List<string>();
+        BitArray xBitArray = new BitArray(65536);
+
+        private void mark_lines()
+        {
+            int xLineNumber, xLp, yLp;
+            string Line, xLine, xCheckString, xChr;
+            int xCheckLength, xAreaStart, xAreaStop;
+            bool xFlag, xCheck;
+
+            if (radioButton1.Checked == true)
+            {
+                xCheckString = "#: " + radioButton1.Text;
+            }
+            else
+            {
+                xCheckString = "#: " + radioButton2.Text;
+            }
+            xCheckLength = xCheckString.Length;
+
+            xLineNumber = xListString.Count;
+            for (xLp = 0; xLp < 65536; xLp++)
+            {
+                xBitArray[xLp] = false;
+            }
+
+            xFlag = false;
+            xCheck = false;
+            xAreaStart = 0;
+            xAreaStop = 0;
+            xLp = 0;
+            for (xLp = 0; xLp < xLineNumber; xLp++)
+            {
+                //                Line = richTextBox1.Lines[xLp];
+                Line = xListString[xLp];
+                if (Line.Length > 4)
+                {
+                    xChr = Line.Substring(0, 3);
+                    if (xChr == @"#: ")
+                    {
+                        if (xFlag == false)
+                        {
+                            xAreaStart = xLp;
+                            xFlag = true;
+                            xCheck = false;
+                        }
+                        if (Line.Length > xCheckLength)
+                        {
+                            xLine = Line.Substring(0, xCheckLength);
+                            if (xLine == xCheckString)
+                            {
+                                xBitArray[xLp] = true;
+                            }
+                            else
+                            {
+                                xCheck = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        xAreaStop = xLp;
+                        if (xCheck == false)
+                        {
+                            for (yLp = xAreaStart; yLp < xAreaStop; yLp++)
+                            {
+                                xBitArray[yLp] = false;
+                            }
+                        }
+                        xFlag = false;
+                    }
+                }
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string line;
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                StreamReader sr = new StreamReader(textBox1.Text, Encoding.UTF8);
+                if (File.Exists(textBox1.Text))
+                {
+                    xListString.Clear();
+                    while ((line = sr.ReadLine()) != null) //Read line of text file 'til end
+                    {
+                        xListString.Add(line);
+                        line += "\r\n";
+                        sb.Append(line);
+                    }
+                    richTextBox1.Text = sb.ToString();
+                }
+                sr.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int xLineNumber, xLp, yLp;
+            string Line, xLine, xPath, xCh;
+            int xLength;
+            bool xEnable, xFull;
+
+            radioButton1.Text = "";
+            radioButton2.Text = "";
+            xPath = "";
+//            xLineNumber = richTextBox1.Lines.Length;
+            xLineNumber = xListString.Count;
+            xFull = false;
+            for (xLp = 0; xLp < xLineNumber; xLp++)
+            {
+                if (xFull == false)
+                {
+                    //                    xLength = richTextBox1.Lines[xLp].Length;
+                    //                    Line = richTextBox1.Lines[xLp];
+                    xLength = xListString[xLp].Length;
+                    Line = xListString[xLp];
+                    if (Line.Length >= 20)
+                    {
+                        xLine = Line.Substring(1, 19);
+                        if (xLine == @"X-Poedit-SearchPath")
+                        {
+                            xEnable = false;
+                            xPath = "";
+                            for (yLp = 19; yLp < xLength; yLp++)
+                            {
+                                xCh = Line.Substring(yLp, 1);
+                                if (xCh == @"/")
+                                {
+                                    xEnable = false;
+                                }
+                                else
+                                {
+                                    if (xEnable == true)
+                                    {
+                                        xPath += xCh;
+                                    }
+                                    if (xCh == @" ")
+                                    {
+                                        xEnable = true;
+                                    }
+                                }
+                            }
+                            if (radioButton1.Text == "")
+                            {
+                                radioButton1.Text = xPath;
+                            }
+                            else
+                            {
+                                if (radioButton1.Text != xPath)
+                                {
+                                    if (radioButton2.Text == "")
+                                    {
+                                        radioButton2.Text = xPath;
+                                        xFull = true;
+                                    }
+                                }
+                            }
+                        }
+                        xLength = 0;
+                    }
+
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int xLineNumber, xLp;
+            int xStart, xLength, xPos;
+
+            mark_lines();
+
+            MessageBox.Show("This feature is debug version only.");
+/*
+            xLineNumber = richTextBox1.Lines.Length;
+            xPos = 0;
+            for (xLp = 0; xLp < xLineNumber; xLp++)
+            {
+                richTextBox1.SelectionLength = 0;
+                xStart = xPos;
+                xLength = richTextBox1.Lines[xLp].Length;
+                richTextBox1.SelectionStart = xStart;
+                richTextBox1.SelectionLength = xLength;
+                if (xBitArray[xLp] == true)
+                {
+                    richTextBox1.SelectionColor = Color.Red;
+                }
+                xPos += xLength + 1;
+                xLength = 0;
+            }
+*/
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int xLineNumber, xLp;
+            string xLine, xMessage;
+
+//            Stopwatch sw = new Stopwatch();   //Start counting.
+//            sw.Start();
+
+            mark_lines();
+
+            xLineNumber = xListString.Count();
+            List<string> xItemList = new List<string>();
+            for (xLp = 0; xLp < xLineNumber; xLp++)
+            {
+                if (xBitArray[xLp] == false)
+                {
+                    xItemList.Add(xListString[xLp]);
+                }
+            }
+
+            xLineNumber = xItemList.Count;
+            richTextBox1.Clear();
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            for (xLp = 0; xLp < xLineNumber; xLp++)
+            {
+                xLine = xItemList[xLp];
+                xLine += "\r\n";
+                sb.Append(xLine);
+            }
+            richTextBox1.Text = sb.ToString();
+
+//            sw.Stop();
+//            xMessage = string.Format("処理終了 : {0:d} msec", sw.ElapsedMilliseconds);    //Report process time.
+//            MessageBox.Show(xMessage);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+//                System.Text.Encoding enc = System.Text.Encoding.GetEncoding("UTF-8");          //UTF-8 with BOM
+//                System.IO.File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text, enc); // 
+                System.IO.File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text);        //UTF-8 without BOM
+            }
+        }
+    }
+}
